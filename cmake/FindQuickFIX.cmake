1a5bd66435c9f1d7a7f62744da050906c46076cb
javagg <lu.lee05@gmail.com>
2012-09-17 00:32:29 +0800

    d

--- cmake/FindQuickFIX.cmake
+++ cmake/FindQuickFIX.cmake
@@ -4,6 +4,8 @@ find_library(QUICKFIX_LIBRARY NAMES quickfix libquickfix HINTS /usr/lib $ENV{QUI
 set(QUICKFIX_LIBRARIES ${QUICKFIX_LIBRARY})
 set(QUICKFIX_INCLUDE_DIRS ${QUICKFIX_INCLUDE_DIR})
 
+# handle the QUIETLY and REQUIRED arguments and set ZLIB_FOUND to TRUE if
+# all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(QUICKFIX DEFAULT_MSG QUICKFIX_LIBRARY QUICKFIX_INCLUDE_DIR)
 mark_as_advanced(QUICKFIX_INCLUDE_DIR QUICKFIX_LIBRARY)
