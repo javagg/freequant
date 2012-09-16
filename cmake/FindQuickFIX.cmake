a00fe8978efbd8b6e511028bcc063200822ea4fa
javagg <lu.lee05@gmail.com>
2012-09-16 17:44:34 +0800

    cmake for win

--- cmake/FindQuickFIX.cmake
+++ cmake/FindQuickFIX.cmake
@@ -1,11 +1,10 @@
-find_path(QuickFIX_INCLUDE_DIR quickfix/FixValues.h HINTS /usr/include)
-find_library(QuickFIX_LIBRARY NAMES quickfix libquickfix HINTS /usr/lib)
+find_path(QUICKFIX_INCLUDE_DIR quickfix/FixValues.h HINTS /usr/include $ENV{QUICKFIX_HOME}/include)
+find_library(QUICKFIX_LIBRARY NAMES quickfix libquickfix HINTS /usr/lib $ENV{QUICKFIX_HOME}/lib)
 
-set(QuickFIX_LIBRARIES ${QuickFIX_LIBRARY})
-set(QuickFIX_INCLUDE_DIRS ${QuickFIX_INCLUDE_DIR})
+set(QUICKFIX_LIBRARIES ${QUICKFIX_LIBRARY})
+set(QUICKFIX_INCLUDE_DIRS ${QUICKFIX_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(QuickFIX  DEFAULT_MSG QUICKFIX_LIBRARY QUICKFIX_INCLUDE_DIR)
-mark_as_advanced(QuickFIX_INCLUDE_DIR QuickFIX_LIBRARY)
-
+find_package_handle_standard_args(QUICKFIX DEFAULT_MSG QUICKFIX_LIBRARY QUICKFIX_INCLUDE_DIR)
+mark_as_advanced(QUICKFIX_INCLUDE_DIR QUICKFIX_LIBRARY)
 
